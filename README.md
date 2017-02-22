be5488bd4bc7d6d8ce6427ca87980ea295f3fd9e
Iremnur Tokac <itokac@localhost.localdomain>
2017-02-22 09:23:50 +0100

    https://github.com/iremnurtokac/tree.git

---
+++ README.md
@@ -0,0 +1,25 @@
+# My favourite Titel
+
+best master thesis ever
+
+## some Subtitel
+
+here is important stuff
+
+* favourite
+* list
+* of
+* things
+    * sub points
+    * another
+
+1. numbered list
+2. second thign
+    * subpoint
+
+![alt](https://s-media-cache-ak0.pinimg.com/474x/eb/af/b9/ebafb95dfc67635a3314580bbdb43816.jpg)
+
+some | table | for
+:---|:---:|---:
+data| i|want
+to |show | here
--- gosandbox.go
+++ gosandbox.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	
 )
 
+func write(comp string, count int) {
+	for i := 0; i < count; i++ {
+		fmt.Print(comp)
+	}
+}
+
 func main() {
-
-	var a int
-
-	a += 1
-	a += 1
-	a += 1
-	a += 1
-
-	fmt.Println(a)
-
+	n := 4
+	
+	for k := 0; k < n; k++ {
+	
+		for i := k; i<n+2*k; i++ {
+		
+			spacecount := (n-i)+(2*n-2)
+			xcount := (2*i +1)
+			write(" ", spacecount)
+			write("x", xcount)
+			fmt.Println()
+		}
+	}
 }
